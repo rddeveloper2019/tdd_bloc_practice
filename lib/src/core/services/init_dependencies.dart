79c3820b3f63a80a8f168f767d53c13808c15f3b
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2026-03-15 02:07:40 +0300

    test and di

---
+++ lib/src/core/services/init_dependencies.dart
@@ -0,0 +1,34 @@
+import 'package:bloc_tdd/src/authentication/authentication.dart';
+import 'package:bloc_tdd/src/authentication/presentation/blocs/cubit/authentication/authentication_cubit.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+
+final GetIt sl = GetIt.instance();
+
+Future<void> init() async {
+  sl
+    //presentation
+    ..registerFactory(
+      () => AuthenticationCubit(
+        createUserUsecase: sl<CreateUserUsecase>(),
+        getUsersUsecase: sl<GetUsersUsecase>(),
+      ),
+    )
+    //usecases
+    ..registerLazySingleton<CreateUserUsecase>(
+      () => CreateUserUsecase(sl<AuthenticationRepository>()),
+    )
+    ..registerLazySingleton<GetUsersUsecase>(
+      () => GetUsersUsecase(sl<AuthenticationRepository>()),
+    )
+    //repositories
+    ..registerLazySingleton<AuthenticationRepository>(
+      () => AuthenticationRepositoryImpl(sl<AuthenticationRemoteDatasource>()),
+    )
+    //datasources
+    ..registerLazySingleton<AuthenticationRemoteDatasource>(
+      () => AuthenticationRemoteDatasourceImpl(sl<http.Client>()),
+    )
+    //external depends
+    ..registerLazySingleton<http.Client>(http.Client.new);
+}
---
+++ test/src/authentication/presentation/cubit/authentication/authentication_cubit_test.dart
@@ -0,0 +1,136 @@
+import 'package:bloc_tdd/src/authentication/authentication.dart';
+import 'package:bloc_tdd/src/authentication/presentation/blocs/cubit/authentication/authentication_cubit.dart';
+import 'package:bloc_tdd/src/core/core.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockGetUsersUsecase extends Mock implements GetUsersUsecase {}
+
+class MockCreateUserUsecase extends Mock implements CreateUserUsecase {}
+
+void main() {
+  late GetUsersUsecase getUsersUsecase;
+  late CreateUserUsecase createUserUsecase;
+  late AuthenticationCubit authenticationCubit;
+  final tCreateUserParams = CreateUserParams.empty();
+  final tApiFailure = ApiFailure(message: 'message', statusCode: 400);
+  setUp(() {
+    getUsersUsecase = MockGetUsersUsecase();
+    createUserUsecase = MockCreateUserUsecase();
+    authenticationCubit = AuthenticationCubit(
+      createUserUsecase: createUserUsecase,
+      getUsersUsecase: getUsersUsecase,
+    );
+  });
+
+  tearDown(() {
+    authenticationCubit.close();
+  });
+
+  test('initial state should be [AuthenticationInitial] ', () async {
+    assert(authenticationCubit.state == const AuthenticationInitial());
+  });
+
+  group('createUser', () {
+    blocTest<AuthenticationCubit, AuthenticationState>(
+      'should emit [CreatingUserState] [CreatedUserState] when successful',
+      // build: () => AuthenticationCubit(
+      //   createUserUsecase: createUserUsecase,
+      //   getUsersUsecase: getUsersUsecase,
+      // ),
+      build: () {
+        when(
+          () => createUserUsecase(tCreateUserParams),
+        ).thenAnswer((_) async => const Right(null));
+        return authenticationCubit;
+      },
+      act: (cubit) {
+        cubit.createUser(
+          createdAt: tCreateUserParams.createdAt,
+          name: tCreateUserParams.name,
+          avatar: tCreateUserParams.avatar,
+        );
+      },
+      expect: () => const <AuthenticationState>[
+        CreatingUserState(),
+        CreatedUserState(),
+      ],
+      verify: (cubit) {
+        verify(() => createUserUsecase(tCreateUserParams)).called(1);
+        verifyNoMoreInteractions(createUserUsecase);
+      },
+    );
+
+    blocTest<AuthenticationCubit, AuthenticationState>(
+      'should emit [CreatingUserState] [AuthenticationErrorState] when unsuccessful',
+
+      build: () {
+        when(
+          () => createUserUsecase(tCreateUserParams),
+        ).thenAnswer((_) async => Left(tApiFailure));
+        return authenticationCubit;
+      },
+      act: (cubit) {
+        cubit.createUser(
+          createdAt: tCreateUserParams.createdAt,
+          name: tCreateUserParams.name,
+          avatar: tCreateUserParams.avatar,
+        );
+      },
+      expect: () => <AuthenticationState>[
+        CreatingUserState(),
+        AuthenticationErrorState(message: tApiFailure.errorMessage),
+      ],
+      verify: (cubit) {
+        verify(() => createUserUsecase(tCreateUserParams)).called(1);
+        verifyNoMoreInteractions(createUserUsecase);
+      },
+    );
+  });
+
+  group('getUsers', () {
+    blocTest<AuthenticationCubit, AuthenticationState>(
+      'should emit [GettingUsersState] [LoadedUsersState] when successful',
+
+      build: () {
+        when(() => getUsersUsecase()).thenAnswer((_) async => const Right([]));
+        return authenticationCubit;
+      },
+      act: (cubit) {
+        cubit.getUsers();
+      },
+      expect: () => const <AuthenticationState>[
+        GettingUsersState(),
+        LoadedUsersState(users: []),
+      ],
+      verify: (cubit) {
+        verify(() => getUsersUsecase()).called(1);
+        verifyNoMoreInteractions(getUsersUsecase);
+      },
+    );
+
+    blocTest<AuthenticationCubit, AuthenticationState>(
+      'should emit [GettingUsersState] [AuthenticationErrorState] when unsuccessful',
+
+      build: () {
+        when(
+          () => getUsersUsecase(),
+        ).thenAnswer((_) async => Left(tApiFailure));
+        return authenticationCubit;
+      },
+      act: (cubit) {
+        cubit.getUsers();
+      },
+      expect: () => <AuthenticationState>[
+        GettingUsersState(),
+        AuthenticationErrorState(message: tApiFailure.errorMessage),
+      ],
+      verify: (cubit) {
+        verify(() => getUsersUsecase()).called(1);
+        verifyNoMoreInteractions(getUsersUsecase);
+      },
+    );
+  });
+}
